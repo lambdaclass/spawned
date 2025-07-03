0a78b6416c13592ec39f25cbaf63f5500d504359
Juan Munoz <juanmunoz890@gmail.com>
2025-07-03 12:38:13 -0300

    merge duplicated code

--- concurrency/src/threads/stream.rs
+++ concurrency/src/threads/stream.rs
@@ -37,11 +37,8 @@ pub fn spawn_listener_from_iter<T, F, S, I, E>(
 /// Spawns a listener that listens to a stream and sends messages to a GenServer.
 ///
 /// Items sent through the stream are required to be wrapped in a Result type.
-pub fn spawn_listener<T, F, S, I, E>(
-    mut handle: GenServerHandle<T>,
-    message_builder: F,
-    mut stream: S,
-) where
+pub fn spawn_listener<T, F, S, I, E>(handle: GenServerHandle<T>, message_builder: F, mut stream: S)
+where
     T: GenServer + 'static,
     F: Fn(I) -> T::CastMsg + Send + 'static,
     I: Send + 'static,
@@ -50,18 +47,5 @@ pub fn spawn_listener<T, F, S, I, E>(
 {
     // Convert the stream into an iterator that blocks on each item.
     let iterable = iter::from_fn(move || futures::executor::block_on(stream.next()));
-    thread::spawn(move || {
-        for res in iterable {
-            match res {
-                Ok(i) => {
-                    let _ = handle.cast(message_builder(i));
-                }
-                Err(err) => {
-                    tracing::error!("Error in stream: {:?}", err);
-                    break;
-                }
-            }
-        }
-        tracing::trace!("Stream finished");
-    });
+    spawn_listener_from_iter(handle, message_builder, iterable);
 }
