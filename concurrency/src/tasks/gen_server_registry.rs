ee3decb9e584f765fa4ecad408e62ef275b545b7
Juan Munoz <juanmunoz890@gmail.com>
2025-07-24 12:44:39 -0300

    add `all_entries` & tests

--- concurrency/src/tasks/gen_server_registry.rs
+++ concurrency/src/tasks/gen_server_registry.rs
@@ -28,12 +28,27 @@ impl<G: GenServer + 'static> AnyGenServerHandle for GenServerHandle<G> {
     }
 }
 
+// Global registry for GenServer handles.
+// This allows us to store and retrieve `GenServerHandle` instances
+// by their address, similar to how PIDs work in Erlang/Elixir.
+//
+// We do not expose this directly, but rather through the `GenServerRegistry` struct.
 static GENSERVER_REGISTRY: OnceCell<Arc<DashMap<String, Box<dyn AnyGenServerHandle>>>> =
     OnceCell::new();
 
+/// A registry for `GenServer` instances, allowing them to be stored and retrieved by address.
+/// This is similar to the PID registry in Erlang/Elixir, where processes can be registered and looked up by their identifiers.
 pub struct GenServerRegistry;
 
 impl GenServerRegistry {
+    /// Adds a `GenServerHandle` to the registry under the specified address.
+    /// Returns an error if the address is already taken.
+    ///
+    /// Example usage:
+    /// ```rs
+    ///     let banana_fruit_basket = FruitBasket::new("Banana", 10).start(); // FruitBasket implements GenServer
+    ///     GenServerRegistry::add_entry("banana_fruit_basket", banana_fruit_basket).unwrap();
+    /// ```
     pub fn add_entry<G: GenServer + 'static>(
         address: &str,
         server_handle: GenServerHandle<G>,
@@ -46,6 +61,14 @@ impl GenServerRegistry {
         Ok(())
     }
 
+    /// Retrieves a `GenServerHandle` from the registry by its address.
+    /// Returns an error if the address is not found or if the handle cannot be downcast
+    ///
+    /// Calling this function requires that the type of `GenServer` retrieved is known at compile time:
+    /// ```rs
+    ///     let mut retrieved_vegetable_basket: GenServerHandle<VegetableBasket> =
+    ///         GenServerRegistry::get_entry("lettuce_vegetable_basket").unwrap();
+    /// ```
     pub fn get_entry<G: GenServer + 'static>(
         address: &str,
     ) -> Result<GenServerHandle<G>, GenServerRegistryError> {
@@ -57,6 +80,41 @@ impl GenServerRegistry {
             .downcast_ref::<GenServerHandle<G>>()
             .cloned()
             .ok_or(GenServerRegistryError::ServerNotFound)
+    }
+
+    /// Retrieves all entries of a specific `GenServer` type from the registry.
+    ///
+    /// Example usage:
+    /// ```rs
+    ///    let fruit_entries: Vec<GenServerHandle<FruitBasket>> =
+    ///        GenServerRegistry::all_entries::<FruitBasket>();
+    /// ```
+    pub fn all_entries<G: GenServer + 'static>() -> Vec<GenServerHandle<G>> {
+        let registry = GENSERVER_REGISTRY.get_or_init(|| Arc::new(DashMap::new()));
+        registry
+            .iter()
+            .filter_map(|entry| {
+                entry
+                    .value()
+                    .as_any()
+                    .downcast_ref::<GenServerHandle<G>>()
+                    .cloned()
+            })
+            .collect()
+    }
+
+    /// Updates an existing entry in the registry with a new `GenServerHandle`.
+    /// Returns an error if the address is not found.
+    pub fn update_entry<G: GenServer + 'static>(
+        address: &str,
+        server_handle: GenServerHandle<G>,
+    ) -> Result<(), GenServerRegistryError> {
+        let registry = GENSERVER_REGISTRY.get_or_init(|| Arc::new(DashMap::new()));
+        if !registry.contains_key(address) {
+            return Err(GenServerRegistryError::ServerNotFound);
+        }
+        registry.insert(address.to_string(), Box::new(server_handle));
+        Ok(())
     }
 }
 
@@ -145,6 +203,9 @@ mod tests {
         }
     }
 
+    // This test checks the functionality of the GenServerRegistry,
+    // It's done all in a signle test function to prevent the global registry
+    // state from not being reset between tests.
     #[test]
     fn test_gen_server_registry() {
         let runtime = rt::Runtime::new().unwrap();
@@ -186,6 +247,50 @@ mod tests {
             } else {
                 panic!("Expected OutMsg::Count");
             }
+
+            // Next we check that we can retrieve all entries
+            let mut amount_collected = 0;
+            let fruit_entries: Vec<GenServerHandle<FruitBasket>> =
+                GenServerRegistry::all_entries::<FruitBasket>();
+            for mut entry in fruit_entries {
+                let out_msg = entry.call(InMsg::GetCount).await.unwrap();
+                let OutMsg::Count(count) = out_msg;
+                amount_collected += count;
+            }
+
+            let vegetable_entries: Vec<GenServerHandle<VegetableBasket>> =
+                GenServerRegistry::all_entries::<VegetableBasket>();
+            for mut entry in vegetable_entries {
+                let out_msg = entry.call(InMsg::GetCount).await.unwrap();
+                let OutMsg::Count(count) = out_msg;
+                amount_collected += count;
+            }
+
+            assert_eq!(amount_collected, 10 + 20);
+
+            // Now we update the entry for the fruit basket
+            let new_banana_fruit_basket = FruitBasket::new("Banana", 15).start();
+            assert!(GenServerRegistry::update_entry(
+                "banana_fruit_basket",
+                new_banana_fruit_basket
+            )
+            .is_ok());
+
+            let mut updated_fruit_basket: GenServerHandle<FruitBasket> =
+                GenServerRegistry::get_entry("banana_fruit_basket").unwrap();
+            let updated_call_result = updated_fruit_basket.call(InMsg::GetCount).await.unwrap();
+            let OutMsg::Count(count) = updated_call_result;
+            assert_eq!(count, 15);
+
+            // Finally, we check that we can't retrieve a non-existent entry
+            assert!(GenServerRegistry::get_entry::<FruitBasket>("orange_fruit_basket").is_err());
+
+            // And neither update a non-existent entry
+            let orange_fruit_basket = FruitBasket::new("Orange", 5).start();
+            assert!(
+                GenServerRegistry::update_entry("orange_fruit_basket", orange_fruit_basket)
+                    .is_err()
+            );
         });
     }
 }
