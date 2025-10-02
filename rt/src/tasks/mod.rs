50aeef76ec8e0e6118a85322ac34713dd4cf85f3
Esteban Dimitroff Hodi <esteban.dimitroff@lambdaclass.com>
2025-10-02 09:13:49 -0300

    Added task_id to warn-on-block message

--- rt/src/tasks/mod.rs
+++ rt/src/tasks/mod.rs
@@ -18,7 +18,7 @@ pub use crate::tasks::tokio::oneshot;
 pub use crate::tasks::tokio::sleep;
 pub use crate::tasks::tokio::timeout;
 pub use crate::tasks::tokio::CancellationToken;
-pub use crate::tasks::tokio::{spawn, spawn_blocking, JoinHandle, Runtime};
+pub use crate::tasks::tokio::{spawn, spawn_blocking, task_id, JoinHandle, Runtime};
 pub use crate::tasks::tokio::{BroadcastStream, ReceiverStream};
 use std::future::Future;
 
